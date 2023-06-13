a50ec860370b285f66e186dc466882565b74cf49
shivanand1602 <shivanandp187@gmail.com>
2023-06-13 12:59:25 +0530

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,34 @@
+const User=require('../models/user')
+
+exports.createOrUpdateUser=async(req,res)=>{    
+    const {name,picture,email} =req.user;
+
+    const user=await User.findOneAndUpdate({email},
+         {name: email.split('@')[0],picture}, 
+         {new:true})
+
+    if(user){
+        console.log('USER UPDATED',user)
+        res.json(user)
+    }else{
+        const newUser=await new User({
+            email,
+            name: email.split('@')[0],
+            picture,
+        }).save();
+        console.log('USER CREATED',newUser);
+        res.json(newUser);
+    }
+};
+
+exports.currentUser=async(req,res)=>{
+    try{
+        const user=await User.findOne({email:req.user.email}).exec();
+        res.json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ error: 'An error occurred while fetching the currentUser.' });
+    }
+};  
+
---
+++ server.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const {readdirSync}=require("fs");
+require('dotenv').config();
+
+
+
+//app
+const app=express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({limit:"2mb"}));
+app.use(cors());
+
+//routes middleware
+
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r))); 
+
+//port
+const port=process.env.port || 8000;
+app.listen(port,()=>console.log(`Server is running on the port ${port}`));
+
